d57d977d464227deb3d1df64f08205a86f4bd075
Kw1liache <zagoskinmaxii@yandex.ru>
2026-01-17 13:55:19 +0500

    Update api.js

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -1,1 +1,11 @@
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-202",
+  headers: {
+    authorization: "86601be9-69ff-4794-8807-2f40ff4bca94",
+    "Content-Type": "application/json",
+  },
+};
 
+const getResponseData = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}; 
