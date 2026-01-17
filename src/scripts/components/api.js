34f41ae11f53a4892ec8e82dd37c7f049581d151
Kw1liache <zagoskinmaxii@yandex.ru>
2026-01-17 13:55:38 +0500

    Update api.js

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -9,3 +9,9 @@ const config = {
 const getResponseData = (res) => {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 }; 
+
+export const getUserInfo = () => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers, 
+  }).then(getResponseData);  
+};
